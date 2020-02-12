2ff7accb60a47dc36bdc8af411d7d2cb715622bf
YCaptain <506840597@qq.com>
2020-02-12 21:57:36 +0800

    feat(ids): add submodule

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -7,26 +7,26 @@ let styles = require('./Home.less');
 
 const Home = (): JSX.Element => {
   // test python script
-  let pyProc = null;
-  const createPyProc = () => {
-    console.warn(remote.app.getAppPath());
-    let script = path.join(remote.app.getAppPath(), '..', 'py', 'test.py');
-    console.log(script);
-    let ch = require('child_process');
-    pyProc = ch.spawn('python', [script])
-    if (pyProc != null) {
-      console.log('child process success')
-      pyProc.stdout.on('data', (data: any) => {
-        console.log(`stdout: ${data}`);
-        new Notification('python', {
-          body: `stdout: ${data}`
-        })
-      });
-    } else {
-      console.log('hello failed');
-    }
-  }
-  createPyProc();
+  // let pyProc = null;
+  // const createPyProc = () => {
+  //   console.warn(remote.app.getAppPath());
+  //   let script = path.join(remote.app.getAppPath(), '..', 'py', 'test.py');
+  //   console.log(script);
+  //   let ch = require('child_process');
+  //   pyProc = ch.spawn('python', [script])
+  //   if (pyProc != null) {
+  //     console.log('child process success')
+  //     pyProc.stdout.on('data', (data: any) => {
+  //       console.log(`stdout: ${data}`);
+  //       new Notification('python', {
+  //         body: `stdout: ${data}`
+  //       })
+  //     });
+  //   } else {
+  //     console.log('hello failed');
+  //   }
+  // }
+  // createPyProc();
   return (
     <div>
       <div className={styles.container} data-tid="container">
