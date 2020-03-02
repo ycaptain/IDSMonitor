e516463a7102b2b6930b34c4b5726f4eb10978c4
YCaptain <506840597@qq.com>
2020-03-02 10:31:41 +0800

    feat(plot): add g2plot

--- app/components/Pred.tsx
+++ app/components/Pred.tsx
@@ -1,7 +1,34 @@
-import * as React from 'react';
-import { Statistic, Card, Row, Col, Icon } from 'antd';
+import React, { useRef, useEffect } from 'react';
+import { Statistic, Card, Row, Col } from 'antd';
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Bar } from '@antv/g2plot';
 
 const Pred = (): JSX.Element => {
+
+  const data = [
+    { year: '1951 年', sales: 38 },
+    { year: '1952 年', sales: 52 },
+    { year: '1956 年', sales: 61 },
+    { year: '1957 年', sales: 145 },
+    { year: '1958 年', sales: 48 },
+  ];
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (!container.current) {
+      return;
+    }
+    const bar = new Bar(container.current!, {
+      data,
+      xField: 'sales',
+      yField: 'year',
+      colorField: 'year',
+    });
+    bar.render();
+  }, []);
+
+
 
   const Data = () => {
     return (
@@ -13,7 +40,7 @@ const Pred = (): JSX.Element => {
               value={13.14}
               precision={2}
               valueStyle={{ color: '#3f8600'}}
-              prefix={<Icon type="arrow-down" />}
+              prefix={<ArrowDownOutlined />}
               suffix="%"
             />
           </Card>
@@ -25,11 +52,12 @@ const Pred = (): JSX.Element => {
             value={56.66}
             precision={2}
             valueStyle={{ color: '#cf1322' }}
-            prefix={<Icon type="arrow-up" />}
+            prefix={<ArrowUpOutlined />}
             suffix="%"
             />
           </Card>
         </Col>
+        <div ref={container}></div>
       </Row>
     );
   }
